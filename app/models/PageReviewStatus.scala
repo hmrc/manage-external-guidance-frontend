2b0ed5df8664646f359068703eefb9216b2662a3
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2020-05-18 14:52:06 +0100

    EG-475, 2i review view with dummy data from controller

---
+++ app/models/PageReviewStatus.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+sealed trait PageReviewStatus 
+
+object PageReviewStatus extends Enumerable.Implicits {
+
+  case object NotStarted extends WithName("NotStarted") with PageReviewStatus
+  case object Reviewed extends WithName("Reviewed") with PageReviewStatus
+
+  val values: Seq[PageReviewStatus] = Seq(NotStarted, Reviewed)
+
+  implicit val enumerable: Enumerable[PageReviewStatus] = Enumerable(values.map(v => v.toString -> v): _*)
+}
