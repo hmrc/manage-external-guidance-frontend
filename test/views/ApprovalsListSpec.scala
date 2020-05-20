ec67a3ed32e918d5dd7a8aaeab1343e6c80034cd
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2020-05-20 10:16:14 +0100

    EG-476, fixed tests

--- test/views/ApprovalsListSpec.scala
+++ test/views/ApprovalsListSpec.scala
@@ -81,10 +81,10 @@ class ApprovalsListSpec extends ViewSpecBase {
             cellData(0).text shouldBe s.title
             Option(cellData(0).getElementsByTag("a").first).fold(fail("Missing link from page url cell")){ a =>
               elementAttrs(a).get("href").fold(fail("Missing href attribute within anchor")){ href =>
-                href.text should be s"/external-guidance/2i-review/${s.id}"
+                href shouldBe s"/external-guidance/2i-review/${s.id}"
               }
             }
-            cellData(1).text shouldBe= s.lastUpdated.format(DateTimeFormatter.ofPattern("dd MMMM yyyy"))
+            cellData(1).text shouldBe s.lastUpdated.format(DateTimeFormatter.ofPattern("dd MMMM yyyy"))
             cellData(2).text shouldBe messages(s"approvalsStatus.${s.status.toString}")
         }
       }
