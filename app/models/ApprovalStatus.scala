bddf974be7e6d1c24ad987f8ed2da9a014a93539
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2020-06-03 12:14:17 +0100

    EG-531, moved Radio creation into view

--- app/models/ApprovalStatus.scala
+++ app/models/ApprovalStatus.scala
@@ -16,11 +16,6 @@
 
 package models
 
-import play.api.data.Form
-import play.api.i18n.Messages
-import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Text
-import uk.gov.hmrc.govukfrontend.views.viewmodels.radios.RadioItem
-
 sealed trait ProcessStatus
 
 sealed trait ApprovalStatus extends ProcessStatus
@@ -36,27 +31,4 @@ object ApprovalStatus extends Enumerable.Implicits {
 
   implicit val enumerable: Enumerable[ApprovalStatus] = Enumerable(values.map(v => v.toString -> v): _*)
 
-  def get2iReviewResultRadioItems(form: Form[_])(implicit messages: Messages): Seq[RadioItem] = {
-
-    Seq(getSend2iReviewResponsesToDesignerRadioItem(form), getApprovedForPublishingRadioItem(form))
-  }
-
-  private def getSend2iReviewResponsesToDesignerRadioItem(form: Form[_])(implicit messages: Messages): RadioItem = {
-
-    RadioItem(
-      value = Some(WithDesignerForUpdate.toString),
-      content = Text(messages(s"2iReviewResult.${WithDesignerForUpdate.toString}")),
-      checked = form("value").value.contains(WithDesignerForUpdate.toString)
-    )
-
-  }
-
-  private def getApprovedForPublishingRadioItem(form: Form[_])(implicit messages: Messages): RadioItem = {
-
-    RadioItem(
-      value = Some(ApprovedForPublishing.toString),
-      content = Text(messages(s"2iReviewResult.${ApprovedForPublishing.toString}")),
-      checked = form("value").value.contains(ApprovedForPublishing.toString)
-    )
-  }
 }
