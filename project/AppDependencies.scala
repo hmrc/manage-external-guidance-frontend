0a93c5222e0bd7f8d8f709f036826177ba0673fa
LesWilson <1684505+LesWilson@users.noreply.github.com>
2021-01-15 13:39:29 +0000

    updating bootstrap plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"             %% "play-language"            % "4.7.0-play-26",
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.16.0",
+    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "2.3.0",
     "uk.gov.hmrc"             %% "play-frontend-govuk"      % "0.57.0-play-26",
     "uk.gov.hmrc"             %% "play-frontend-hmrc"       % "0.17.0-play-26",
     "uk.gov.hmrc"             %% "auth-client"              % "3.2.0-play-26",
@@ -12,7 +12,7 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "1.16.0" % Test classifier "tests",
+    "uk.gov.hmrc"             %% "bootstrap-play-26"        % "2.3.0" % Test classifier "tests",
     "org.scalamock"           %% "scalamock"                % "4.4.0"                 % "test",
     "org.scalatest"           %% "scalatest"                % "3.0.8"                 % "test",
     "org.jsoup"               %  "jsoup"                    % "1.10.2"                % "test",
