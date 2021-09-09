07c3035f13203616debc8f9038c76675b4a237cb
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2021-09-09 11:32:36 +0100

    DL-6197e, CORS updates for Ocelot dev

--- test/controllers/apis/TimescalesControllerSpec.scala
+++ test/controllers/apis/TimescalesControllerSpec.scala
@@ -102,7 +102,7 @@ class TimescalesControllerSpec extends BaseSpec with GuiceOneAppPerSuite with Mo
 
   }
 
-  "OPTIONS /scratch" should {
+  "OPTIONS /timescales" should {
     "return 200" in {
       val result = controller.timescaleOptions()(fakeRequest)
       status(result) shouldBe Status.OK
@@ -110,7 +110,7 @@ class TimescalesControllerSpec extends BaseSpec with GuiceOneAppPerSuite with Mo
 
     "return CORS headers" in {
       val expectedHeaders = Map(
-        "Access-Control-Allow-Origin" -> "https://cc-cdio.guidance.prod.dop.corp.hmrc.gov.uk",
+        "Access-Control-Allow-Origin" -> "https://scratch.guidance.prod.dop.corp.hmrc.gov.uk",
         "Access-Control-Allow-Headers" -> "content-type, Authorization",
         "Access-Control-Allow-Methods" -> "POST, OPTIONS",
         "Access-Control-Allow-Credentials" -> "true"
