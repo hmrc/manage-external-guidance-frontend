38d3712d133301acf547e7eebd7e5a4d204265fe
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2022-11-25 14:43:58 +0000

    DL-8973, pr.commenter and dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,13 @@ import sbt._
 
 object AppDependencies {
   val compile = Seq(
-    "uk.gov.hmrc"             %% "play-language"              % "5.3.0-play-28",
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.25.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "3.22.0-play-28"
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "3.34.0-play-28"
   )
 
   val test = Seq(
     "org.scalamock"                %% "scalamock"                % "5.2.0"   % "test",
-    "org.jsoup"                    %  "jsoup"                    % "1.15.2"  % "test",
+    "org.jsoup"                    %  "jsoup"                    % "1.15.3"  % "test",
     "com.typesafe.play"            %% "play-test"                % current   % "test, it",
     "org.pegdown"                  %  "pegdown"                  % "1.6.0"   % "test, it",
     "com.github.tomakehurst"       %  "wiremock-jre8"            % "2.33.2"  % "test, it",
