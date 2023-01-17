e9e8bf2d2c6ce9674ad35e8af3a6d469d6b708ba
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2023-01-17 14:13:30 +0000

    DL-9505 update play frontend hmrc to latest

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt._
 
 object AppDependencies {
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.25.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "4.1.0-play-28"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "5.25.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "6.2.0-play-28"
   )
 
   val test = Seq(
