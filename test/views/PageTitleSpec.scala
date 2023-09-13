3eee2017d7697a16a9f49001683e71b4f99ad1a9
christopher.richardson <55755517+cmrhmrc@users.noreply.github.com>
2023-09-13 09:10:34 +0100

    DL-11537, removed commented out test

--- test/views/PageTitleSpec.scala
+++ test/views/PageTitleSpec.scala
@@ -109,11 +109,5 @@ class PageTitleSpec extends ViewSpecBase {
       checkTitleWithError(asDocument(view("","", "", form, 1)(FakeRequest("GET", "/blah"), messages)), Some(messages("2iReview.heading")))
     }
 
-    // "Render correct twoeye_review_result title" in {
-    //   val view = injector.instanceOf[twoeye_review_result]
-    //   val formProvider = new TwoEyeReviewResultFormProvider()
-    //   checkTitle(asDocument(view("", formProvider())(FakeRequest("GET", "/blah"), messages)), Some(messages("2iReview.heading")))
-    // }
- 
   }
 }
