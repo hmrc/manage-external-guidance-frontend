5609d06493e8521fecec192cb1ed2e29a6410e36
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2023-12-15 18:15:35 +0000

    DL-12281, dependency updates and warnings removal

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,17 +3,17 @@ import sbt._
 
 object AppDependencies {
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "8.1.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % "8.2.0",
     "uk.gov.hmrc" %% "play-frontend-hmrc"         % "7.29.0-play-28"
   )
 
   val test = Seq(
     "org.scalamock"                %% "scalamock"                % "5.2.0"   % "test",
-    "org.jsoup"                    %  "jsoup"                    % "1.16.1"  % "test",
+    "org.jsoup"                    %  "jsoup"                    % "1.17.1"  % "test",
     "com.typesafe.play"            %% "play-test"                % current   % "test, it",
-    "com.github.tomakehurst"       %  "wiremock-jre8"            % "2.35.0"  % "test, it",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"     % "2.15.2"  % "test, it",
-    "uk.gov.hmrc"                  %% "bootstrap-test-play-28"   % "8.1.0"  % "test, it"
+    "com.github.tomakehurst"       %  "wiremock-jre8"            % "2.35.1"  % "test, it",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"     % "2.16.0"  % "test, it",
+    "uk.gov.hmrc"                  %% "bootstrap-test-play-28"   % "8.2.0"  % "test, it"
   )
 
 }
