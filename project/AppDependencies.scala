88edd6b032551dbabea51df782f093a39b366a44
Paul Anderson <15211314+paulanderson1234@users.noreply.github.com>
2024-02-19 13:44:06 +0000

    DL-13043 bump up play frontend hmrc version to include new crown logo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,12 +5,12 @@ object AppDependencies {
 
   val bootstrapVersion = "8.2.0"
 
-  val compile = Seq(
+  val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc"         % "7.29.0-play-28"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-28" % "8.5.0"
   )
 
-  val test = Seq(
+  val test: Seq[ModuleID] = Seq(
     "org.scalamock"                %% "scalamock"                % "5.2.0"   % "test",
     "org.jsoup"                    %  "jsoup"                    % "1.17.1"  % "test",
     "com.typesafe.play"            %% "play-test"                % current   % "test",
@@ -19,9 +19,8 @@ object AppDependencies {
     "uk.gov.hmrc"                  %% "bootstrap-test-play-28"   % bootstrapVersion  % "test"
   )
 
-  val itDependencies = Seq(
+  val itDependencies: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-test-play-28" % bootstrapVersion  % Test,
-    "org.pegdown"                   %  "pegdown"                % "1.6.0"           % Test,
     "com.github.tomakehurst"        % "wiremock-jre8"           % "2.35.1"          % Test,
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"   % "2.16.0"          % Test,
     "uk.gov.hmrc"                   %% "bootstrap-test-play-28" % bootstrapVersion  % Test
