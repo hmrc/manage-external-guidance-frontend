8cea1a1b5708aefa5c02ed52e24ec51268bb70e5
rameshmandanapu1 <155470931+rameshmandanapu1@users.noreply.github.com>
2024-02-28 10:39:05 +0000

    DL-12498 -Wiremock plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -17,7 +17,7 @@ object AppDependencies {
   val itDependencies: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-test-play-28"     % bootstrapVersion  % Test,
     "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "8.5.0" % Test,
-    "com.github.tomakehurst"       %  "wiremock-standalone"            % "3.0.1"  % "test",
+    "org.wiremock"       %  "wiremock"            % "3.4.2"  % Test
   )
 
 }
