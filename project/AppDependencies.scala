06e8672b25efc8894e01391f408d61dd6bebeb02
cmrhmrc <55755517+cmrhmrc@users.noreply.github.com>
2024-07-15 18:04:58 +0100

    DL-14066, depndency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,15 +6,14 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "10.2.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "10.4.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalamock"                %% "scalamock"                % "5.2.0"   % "test",
-    "uk.gov.hmrc"                  %% "bootstrap-test-play-30"   % bootstrapVersion  % "test",
-    "org.scalatestplus.play"        %% "scalatestplus-play"     % "7.0.1"          % "test",
-    "org.scalatestplus"             %% "mockito-3-4"            % "3.2.10.0"       % "test",
-    "org.mockito"                   % "mockito-core"            % "5.10.0"         % "test"
+    "org.scalamock"                %% "scalamock"                % "5.2.0"          % "test",
+    "uk.gov.hmrc"                  %% "bootstrap-test-play-30"   % bootstrapVersion % "test",
+    "org.scalatestplus"            %% "mockito-3-4"              % "3.2.10.0"       % "test",
+    "org.mockito"                  %  "mockito-core"             % "5.12.0"         % "test"
   )
 
   val itDependencies = Seq(
